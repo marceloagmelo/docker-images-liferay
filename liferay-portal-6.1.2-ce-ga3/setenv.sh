f28f2d1a7d135ad418af631e7f7a59013054c92b
Marcelo Melo <marceloagmelo@gmail.com>
2016-07-15 13:54:21 -0300

    configuração de cluster

--- liferay-portal-6.1.2-ce-ga3/setenv.sh
+++ liferay-portal-6.1.2-ce-ga3/setenv.sh
@@ -1,1 +1,1 @@
-JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT-3 -Xmx1024m -XX:MaxPermSize=256m"
+JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT-3 -Xmx1024m -XX:MaxPermSize=256m"
